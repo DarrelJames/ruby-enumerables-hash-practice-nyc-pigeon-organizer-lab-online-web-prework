5ee2a880dd26beff284a66d2b0515091168a29be
Darrel C <dcastellano2007@gmail.com>
2019-08-25 17:07:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,19 @@
 def nyc_pigeon_organizer(data)
+  # write your code here!
   new_hash = {}
   data.each do |key, value|
     value.each do |new_value, names|
       names.each do |name|
+        # puts name
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
 
+        if !new_hash[name][key]
+          new_hash[name][key] = new_value
+        end
       end
     end
   end
+  new_hash
 end
